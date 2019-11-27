fc91b858717ffc92b7b3a2fb9baf4ed4723948c3
Andrea Busacca <busaccandrea@gmail.com>
2019-11-27 12:47:39 +0100

    graph is now directed
    edge weigth changed.

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def get_jaccard_index(set_a, set_b):
 
     return jaccard_index
 
+def get_weight(set_a, set_b):
+    set_a = next(iter(set_a)).sim
+    set_b = next(iter(set_b)).sim
+    inters = len(set_a.intersection(set_b))
+    weight = inters/len(set_a)
+    return weight
+
+
 # set parameters
 x_shape = 91
 y_shape = 75
@@ -19,7 +27,7 @@ csv_filename = "scaled_map25x25.csv" # set shape to x=91, y=75
 #csv_filename = "graph_test.csv" # set shape to x=5, y=4
 
 print("Creating Graph...")
-G = nx.Graph()
+G = nx.DiGraph()
 print("Done.")
 our_map = utility.load_csv_map(shapes=[x_shape, y_shape], map_filename = ".\\CSVMaps\\"+csv_filename)
 list_node = prx.aggregate(our_map)
@@ -27,10 +35,12 @@ list_node = prx.aggregate(our_map)
 print("Adding nodes...")
 id_node = 0
 for node in list_node:
-    list_region = set(node[0])
-    list_neighbors = set(node[1])
-    G.add_node(id_node, list_region = list_region, list_neighbors = list_neighbors)
-    id_node += 1
+    if len(node[0][0].sim) != 0:
+        n_sim = len(node[0][0].sim)
+        list_region = set(node[0])
+        list_neighbors = set(node[1])
+        G.add_node(id_node, list_region = list_region, list_neighbors = list_neighbors, n_region = len(list_region), n_sim = n_sim)
+        id_node += 1
 print("Done.")
 
 print("Creating edges...")
@@ -39,19 +49,26 @@ for u in G.nodes():
     for v in G.nodes():
         if u != v:
             if len(G.node[u]["list_region"].intersection(G.node[v]["list_neighbors"])) >= 1:
-                weight = get_jaccard_index(G.node[u]["list_region"], G.node[v]["list_region"])
-                G.add_edge(u,v, weight=weight)
-                n_edges += 1
+                
+                # weight = get_jaccard_index(G.node[u]["list_region"], G.node[v]["list_region"])
+                
+                weight_uv = get_weight(G.node[u]["list_region"], G.node[v]["list_region"])
+                weight_vu = get_weight(G.node[v]["list_region"], G.node[u]["list_region"])
+                # creo l'edge solo se il peso diverso da 0
+                if weight_uv != 0:
+                    G.add_edge(u, v, weight=weight_uv)
+                    G.add_edge(v, u, weight=weight_vu)
+                    n_edges += 2
 
-    if (n_edges % 5000 == 0 and n_edges != 0):
+    if n_edges % 5000 == 0:
         print("Created ", n_edges, " edges.")
 
-G_copy = nx.Graph()
-for u in G.nodes():
+G_copy = nx.DiGraph()
+for u, data in G.nodes(data = True):
     list_region = repr((next(iter(G.node[u]["list_region"])).sim))
-    G_copy.add_node(u, label= list_region)
+    G_copy.add_node(u, label= list_region, n_region = data["n_region"], n_sim = data["n_sim"])
     
-for node1,node2,data in G.edges(data=True):
+for node1, node2, data in G.edges(data=True):
     G_copy.add_edge(node1, node2, weight = data['weight'])
 print("fatto.")
 print("Writing gexf...")
