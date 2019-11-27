61f9461db26bf6adf681cacedc1473c507d3439f
Boros93 <andrea_sequenzia@yahoo.it>
2019-11-27 18:00:21 +0100

    Refactoring
    Create graph_maker.py and main modified

---
+++ graph_maker.py
@@ -0,0 +1,109 @@
+import networkx as nx 
+import processing as prx
+import utility 
+from statistics import median
+
+
+def create_graph(l_map):
+    G = nx.DiGraph()
+    ''' Aggrega le regioni, restituendo una lista formata da altre due liste di tipo Region:
+    - region_list, lista delle regioni che formano il nodo (sim tutte uguali)
+    - near_node_list, lista delle regioni visitate durante l'aggregazione (vicinato) '''
+    list_node = prx.aggregate(l_map)
+    create_nodes(G, list_node)
+    create_edges(G)
+    print("N. nodes: ", G.number_of_nodes())
+    print("N. edges: ", G.number_of_edges())
+    return G
+
+def create_nodes(G, list_node):
+    print("Adding nodes...")
+    id_node = 0
+    for node in list_node:
+        # Si estraggono le due liste per ogni nodo
+        region_list = node[0]
+        near_node_list = node[1]
+        # Se la regione non ha nemmeno una sim, non viene creato il nodo
+        if len(region_list[0].sim) != 0:
+            # Creazione attributi da inserire nel grafo
+            # Numero simulazioni per nodo
+            n_sim = len(region_list[0].sim)
+            # Numero di regioni aggregate
+            n_region = len(region_list)
+            # Coordinate dei baricentri dei nodi per layout
+            x,y = get_median_position(region_list)
+            # Casting in set delle liste, computazionalmente efficienti
+            region_list = set(region_list)
+            near_node_list = set(near_node_list)
+            G.add_node(id_node, region_list = region_list, near_node_list = near_node_list, n_region = n_region, n_sim = n_sim, x = x, y = y)
+            id_node += 1
+    print("Done.")
+
+def create_edges(G):
+    print("Creating edges...")
+    n_edges = 0
+    for u in G.nodes():
+        for v in G.nodes():
+            if u != v:
+                # Se ci sono elementi in comune tra la lista delle regioni di u e i vicini di v, allora viene creato l'edge
+                if len(G.node[u]['region_list'].intersection(G.node[v]['near_node_list'])) >= 1:
+                    # Calcolo pesi degli edge
+                    weight_uv = get_weight(G.node[u]['region_list'], G.node[v]['region_list'])
+                    weight_vu = get_weight(G.node[v]['region_list'], G.node[u]['region_list'])
+                    # Viene creato l'edge solo se il peso è maggiore di 0
+                    if weight_uv != 0:
+                        G.add_edge(u, v, weight=weight_uv)
+                        G.add_edge(v, u, weight=weight_vu)
+                        n_edges += 2
+                    if n_edges % 5000 == 0 and n_edges != 0:
+                        print("Created", n_edges, " edges")
+    print("Done.")
+
+# Metodo per esportare il grafo in formato .gexf (per visualizzare)
+def export_graph(G, filename):
+    print("Writing gexf file...")
+    # Crea una copia del grafo per esportarlo con gli attributi 
+    G_copy = nx.DiGraph()
+    for u, data in G.nodes(data=True):
+        # Fa il cast da set a stringa per darlo come attributo (gephi non accetta set)
+        region_list = repr((next(iter(G.node[u]["region_list"])).sim))
+        G_copy.add_node(u, label = region_list, n_region = data['n_region'], n_sim = data['n_sim'],
+                        x = 89 - int(data['x']), y = int(data['y']))
+    
+    for node1, node2, data in G.edges(data=True):
+        G_copy.add_edge(node1, node2, weight = data['weight'])
+    
+    nx.write_gexf(G_copy, ".\\graph_gexf\\"+filename)
+    print("Writed in ", filename)
+
+# Metodo per calcolare un eventuale peso: inters(a,b)/union(a,b)
+def get_jaccard_index(set_a, set_b):
+    set_a = next(iter(set_a)).sim
+    set_b = next(iter(set_b)).sim
+    inters = len(set_a.intersection(set_b))
+    union = len(set_a.union(set_b))
+    jaccard_index = inters/union
+
+    return jaccard_index
+
+# Metodo per calcolare il peso: inters(a,b)/len(a)
+def get_weight(set_a, set_b):
+    set_a = next(iter(set_a)).sim
+    set_b = next(iter(set_b)).sim
+    inters = len(set_a.intersection(set_b))
+    weight = inters/len(set_a)
+    return weight
+
+# Metodo per calcolare il baricentro di una regione
+def get_median_position(list_region):
+    list_x = []
+    list_y = []
+    for reg in list_region:
+        list_x.append(reg.coord[0])
+        list_y.append(reg.coord[1])
+    median_x = median(list_x)
+    median_y = median(list_y)
+    return median_x, median_y
+
+
+
--- main.py
+++ main.py
@@ -1,96 +1,13 @@
-import networkx as nx
-import processing as prx
+import graph_maker as gm
 import utility
-from statistics import median
-
-# inters(a,b)/union(a,b)
-def get_jaccard_index(set_a, set_b):
-    set_a = next(iter(set_a)).sim
-    set_b = next(iter(set_b)).sim
-    inters = len(set_a.intersection(set_b))
-    union = len(set_a.union(set_b))
-    jaccard_index = inters/union
-
-    return jaccard_index
-
-def get_weight(set_a, set_b):
-    set_a = next(iter(set_a)).sim
-    set_b = next(iter(set_b)).sim
-    inters = len(set_a.intersection(set_b))
-    weight = inters/len(set_a)
-    return weight
-
-def get_median_position(list_region):
-    list_x = []
-    list_y = []
-    for reg in list_region:
-        list_x.append(reg.coord[0])
-        list_y.append(reg.coord[1])
-    median_x = median(list_x)
-    median_y = median(list_y)
-    return median_x, median_y
-
-
-# set parameters
+# Set parameters
 x_shape = 91
 y_shape = 75
-csv_filename = "scaled_map25x25.csv" # set shape to x=91, y=75
+filename = "scaled_map25x25" # set shape to x=91, y=75
 #csv_filename = "graph_test.csv" # set shape to x=5, y=4
-
-print("Creating Graph...")
-G = nx.DiGraph()
-print("Done.")
-our_map = utility.load_csv_map(shapes=[x_shape, y_shape], map_filename = ".\\CSVMaps\\"+csv_filename)
-list_node = prx.aggregate(our_map)
-
-print("Adding nodes...")
-id_node = 0
-for node in list_node:
-    if len(node[0][0].sim) != 0:
-        n_sim = len(node[0][0].sim)
-        x, y = get_median_position(node[0])
-        list_region = set(node[0])
-        list_neighbors = set(node[1])
-        G.add_node(id_node, list_region = list_region, list_neighbors = list_neighbors, n_region = len(list_region), n_sim = n_sim, x = x, y = y)
-        id_node += 1
-print("Done.")
-
-print("Creating edges...")
-n_edges = 0
-for u in G.nodes():
-    for v in G.nodes():
-        if u != v:
-            if len(G.node[u]["list_region"].intersection(G.node[v]["list_neighbors"])) >= 1:
-                
-                # weight = get_jaccard_index(G.node[u]["list_region"], G.node[v]["list_region"])
-                
-                weight_uv = get_weight(G.node[u]["list_region"], G.node[v]["list_region"])
-                weight_vu = get_weight(G.node[v]["list_region"], G.node[u]["list_region"])
-                # creo l'edge solo se il peso diverso da 0
-                if weight_uv != 0:
-                    G.add_edge(u, v, weight=weight_uv)
-                    G.add_edge(v, u, weight=weight_vu)
-                    n_edges += 2
-
-                    if n_edges % 5000 == 0 and n_edges !=0:
-                        print("Created ", n_edges, " edges.")
-
-G_copy = nx.DiGraph()
-for u, data in G.nodes(data = True):
-    list_region = repr((next(iter(G.node[u]["list_region"])).sim))
-    G_copy.add_node(u, label= list_region, n_region = data["n_region"], n_sim = data["n_sim"], x = 89- int(data["x"]), y = int(data["y"]))
-    
-for node1, node2, data in G.edges(data=True):
-    G_copy.add_edge(node1, node2, weight = data['weight'])
-print("fatto.")
-print("Writing gexf...")
-
-gexf_filename=csv_filename[0:-3]+"gexf"
-print("Writed in" ,gexf_filename)
-nx.write_gexf(G_copy, ".\\graph_gexf\\"+gexf_filename)
-print("Done.")
-print("Results: ", G_copy.number_of_nodes(), " nodes, ", G_copy.number_of_edges(), " edges.")
-
-
-
-#print(G.node(0))
+# Carica la linked map csv
+our_map = utility.load_csv_map(shapes=[x_shape, y_shape], map_filename = ".\\CSVMaps\\"+ filename + ".csv")
+# Crea il grafo 
+G = gm.create_graph(our_map)
+# e lo esporta
+gm.export_graph(G, filename + ".gexf")
