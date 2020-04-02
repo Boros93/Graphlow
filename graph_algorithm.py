added precision, accuracy, f1, tpr, ppv. normaliz. eruption. bug fixes
fixed bug in saving ascii grids.
new parameters in matrix to utm method
added new commands

graph_algorithm.py
@@ -2,6 +2,8 @@ import utility
 import queue
 import math
 import random
+from scipy import sparse
+import numpy as np
 
 def set_node_rank(G, not_n_filename):
     # inserisco nella lista il nodo per poter azzerarne il rango alla fine.
@@ -69,7 +71,7 @@ def get_id_from_coord(G, coord):
             if coord_x == int(coord[0]/25) and coord_y == int(coord[1]/25):
                 return u
 
-def eruption(G, id_vent, volume, n_days, alpha, threshold):
+'''def eruption(G, id_vent, volume, n_days, alpha, threshold):
     volume_per_day = int(volume/n_days)
     volume_remaining = volume
     node_to_visit=[]
@@ -116,15 +118,15 @@ def eruption(G, id_vent, volume, n_days, alpha, threshold):
         #print("flusso nel nodo root", G.node[root]["current_flow"])
         node_to_visit = []
         node_to_visit = temp_list
-    return G
+    return G'''
 
 # eruption con controlli su transmitrank e senza controlli su altezze
 # transmit_rank(u, v) - transmit_rank(v, u) > epsilon 
-def eruption1(G, id_vent, volume, n_days, alpha, threshold):
+def eruption1(G, id_vent, volume, n_days, alpha):
     volume_per_day = int(volume/n_days)
     volume_remaining = volume
     node_to_visit=[]
-    coord_vent = utility.vent_in_dem(id_vent)
+    coord_vent = utility.vent_in_dem(id_vent - 1)
     root = get_id_from_coord(G, coord_vent)
     node_to_visit.append(root)
     G.node[root]["current_flow"] = volume_per_day
@@ -135,7 +137,6 @@ def eruption1(G, id_vent, volume, n_days, alpha, threshold):
     while not len(node_to_visit) == 0:
         temp_list = []
         if volume_remaining > 0:
-            print("giorno", day_count)
             G.node[root]["current_flow"] += volume_per_day
             volume_remaining -= volume_per_day
             day_count += 1
@@ -146,12 +147,14 @@ def eruption1(G, id_vent, volume, n_days, alpha, threshold):
                 v_flow = G.node[v]["current_flow"]
                 u_height = G.node[u]["height"]
                 v_height = G.node[v]["height"]
-                delta_h = u_flow - v_flow
-                temp = (u_flow + u_height)/(v_flow + v_height)
+                delta_h = (u_flow + u_height) - (v_flow + v_height)
+                if delta_h < 0:
+                    continue
+                delta_h = min(delta_h, u_flow)
+                temp = (u_flow + u_height)/(v_flow + v_height) - 1
                 if  G.edges[u, v]["trasmittance"] - G.edges[v, u]["trasmittance"] > epsilon:
                     G.edges[u, v]["forwarding_flow"] = G.edges[u, v]["trasmittance"] * alpha * delta_h * (1/ (1 + math.exp(-temp)))
                     if u not in temp_list and G.edges[u, v]["forwarding_flow"] > 0.1:
-                        #print("forwarding flow",G.edges[u, v]["forwarding_flow"], "from", u,"to",v)
                         temp_list.append(u)
         if len(temp_list) > 0:
             for u in node_to_visit:
@@ -159,17 +162,34 @@ def eruption1(G, id_vent, volume, n_days, alpha, threshold):
                     if G.edges[u, v]["forwarding_flow"] > 0:
                         G.node[v]["current_flow"] += G.edges[u, v]["forwarding_flow"]
                         G.node[u]["current_flow"] -= G.edges[u, v]["forwarding_flow"]
-                        #print("passo", G.edges[u, v]["forwarding_flow"],"lava da", u,"a", v)
                         G.edges[u, v]["forwarding_flow"] = 0.0
                         if v not in temp_list:
                             temp_list.append(v)
-                #print("flow nel nodo", G.node[u]["current_flow"])
-        #print("flusso nel nodo root", G.node[root]["current_flow"])
+                
         node_to_visit = []
         node_to_visit = temp_list
+    # esporta l'output come vettore per poi ottenere la matrice sparsa
+    #        necessaria per il calcolo delle metriche
+    vect = np.zeros(len(G.nodes()))
+    max_cf = 0 
+
+    for u in G.nodes():
+        if G.node[u]["current_flow"] > max_cf:
+            max_cf = G.node[u]["current_flow"]
+    for u in G.nodes(): 
+        
+        val = (G.node[u]["current_flow"] - 1.e-7) / (max_cf - 1.e-7)
+        vect[int(u)] = val
+        if vect[int(u)] < 1.e-7:
+            vect[int(u)] = 0
+        if G.node[u]["current_flow"] == max_cf:
+            print(vect[int(u)])
+    
+    sparse_vect = sparse.csr_matrix(vect)
+    sparse.save_npz("sparse/eruption_" + str(id_vent) + ".npz", sparse_vect, compressed = True)
     return G
 
-def eruption_new(G, id_vent, threshold):
+'''def eruption_new(G, id_vent, threshold):
     print(threshold)
     coord_vent = utility.vent_in_dem(id_vent)
     root = get_id_from_coord(G, coord_vent)
@@ -191,15 +211,14 @@ def eruption_new(G, id_vent, threshold):
                     G.node[current_node]['current_flow'] -= flow
                     print(G.node[current_node]['current_flow'])
                 G.node[v]['awash'] = True
-    return G
+    return G'''
 
 
-def eruption_prob(G, id_vent, epoch):
-    coord_vent = utility.vent_in_dem(id_vent)
+def prob_eruption(G, id_vent, epoch):
+    coord_vent = utility.vent_in_dem(id_vent - 1)
     root = get_id_from_coord(G, coord_vent)
     node_to_restart = []
-    for ep in range(0, epoch): 
-        print("Epoch:", ep)
+    for ep in range(0, epoch):
         G.node[root]['awash'] = True
         node_to_visit = queue.Queue()
         node_to_visit.put(root)
@@ -210,15 +229,15 @@ def eruption_prob(G, id_vent, epoch):
             id_max_prob = 0
             for v in G.successors(current_node):
                 if(not G.node[v]['awash']):
-                    rand_value = random.uniform(0, 0.5)
-                    if rand_value < G.edges[current_node, v]["weight"]:
+                    rand_value = random.uniform(0, 1)
+                    if rand_value < G.edges[current_node, v]["trasmittance"]:
                         awashed += 1
                         G.node[v]['awash'] = True
                         G.node[v]['current_flow'] += 1
                         node_to_restart.append(v)
                         node_to_visit.put(v)
-                if G.edges[current_node, v]["weight"] > max_prob:
-                    max_prob = G.edges[current_node, v]["weight"]
+                if G.edges[current_node, v]["trasmittance"] > max_prob:
+                    max_prob = G.edges[current_node, v]["trasmittance"]
                     id_max_prob = v
 
             if id_max_prob != 0:
@@ -230,13 +249,19 @@ def eruption_prob(G, id_vent, epoch):
                             node_to_restart.append(id_max_prob)
                             G.node[id_max_prob]['current_flow'] += 1
                             node_to_visit.put(v)
-                            print(v, "Awashed dopo")
+                            #print(v, "Awashed dopo")
         for node in node_to_restart:
             G.node[node]['awash'] = False
 
 
+    # esporta l'output come vettore per poi ottenere la matrice sparsa
+    #        necessaria per il calcolo delle metriche
+    vect = np.zeros(len(G.nodes()))
     for u in G.nodes():
         G.node[u]['current_flow'] = G.node[u]['current_flow'] / epoch
+        vect[int(u)] = G.node[u]["current_flow"] / epoch
+    sparse_vect = sparse.csr_matrix(vect)
+    sparse.save_npz("sparse/proberuption_" + str(id_vent) + ".npz", sparse_vect, compressed = True)
     return G
 
 #seconda implementazione del metodo eruption

graph_maker.py
@@ -5,6 +5,7 @@ import init_map
 import math
 from statistics import median
 import os
+import numpy as np
 
 
 def create_graph(l_map):
@@ -74,8 +75,10 @@ def create_edges(G):
                     slope_vu = -slope_uv
                     # Viene creato l'edge solo se il peso è maggiore di 0
                     if weight_uv != 0:
-                        G.add_edge(u, v, weight=weight_uv, transmit_rank = 0, slope = slope_uv, forwarding_flow = 0.0, trasmittance = 0.0)
-                        G.add_edge(v, u, weight=weight_vu, transmit_rank = 0, slope = slope_vu, forwarding_flow = 0.0, trasmittance = 0.0)
+                        G.add_edge(u, v, weight=weight_uv, transmit_rank = 0, slope = slope_uv, forwarding_flow = 0.0,
+                                            trasmittance = 0.0, sigmoid_norm_tr_rank = 0.0)
+                        G.add_edge(v, u, weight=weight_vu, transmit_rank = 0, slope = slope_vu, forwarding_flow = 0.0,
+                                            trasmittance = 0.0, sigmoid_norm_tr_rank = 0.0)
                         n_edges += 2
                     if n_edges % 5000 == 0 and n_edges != 0:
                         print("Created", n_edges, " edges")
@@ -84,7 +87,6 @@ def create_edges(G):
 
 # Metodo per esportare il grafo in formato .gexf (per visualizzare)
 def export_graph(G, filename, is_first_time):
-    print("Writing gexf file...")
     # Crea una copia del grafo per esportarlo con gli attributi 
     G_copy = nx.DiGraph()
     for u, data in G.nodes(data=True):
@@ -105,10 +107,10 @@ def export_graph(G, filename, is_first_time):
         
     for node1, node2, data in G.edges(data=True):
         G_copy.add_edge(node1, node2, weight = data['weight'], transmit_rank = data["transmit_rank"], slope = data["slope"],
-                        forwarding_flow = data["forwarding_flow"], trasmittance = data["trasmittance"])
+                        forwarding_flow = data["forwarding_flow"], trasmittance = data["trasmittance"],
+                        sigmoid_norm_tr_rank = data ["sigmoid_norm_tr_rank"])
 
     nx.write_gexf(G_copy, "./graph_gexf/eruption/" + filename)
-    print("Writed in ", filename)
     return G_copy
 
 # Metodo per calcolare un eventuale peso: inters(a,b)/union(a,b)
@@ -175,6 +177,28 @@ def normalize_trasmittance(G):
             for v in G.successors(u):
                 G.edges[u, v]["trasmittance"] =  G.edges[u, v]["transmit_rank"] / denominator
     return G
+
+def sigmoid_norm_tr_rank(G):
+    for u in G.nodes():
+        for v in G.successors(u):
+            u_v_trasm = G.edges[u, v]["transmit_rank"]
+            if u_v_trasm > 0:
+                pol_ratio = polynomial_ratio(u_v_trasm)
+                sigmoid_norm_tr_rank = sigmoid(pol_ratio)
+                G.edges[u, v]["sigmoid_norm_tr_rank"] =  float(sigmoid_norm_tr_rank)
+    return G
+
+# f(x) = 41*ln(3)(x - 22)/(25*x+506), dove x = trasmit rank di (u,v)
+def polynomial_ratio(trasmit_rank):
+    N = (trasmit_rank - 22) * 41 * np.log(3)
+    D = (25 * trasmit_rank) + 506
+    pol_ratio = N / D
+    return pol_ratio
+
+def sigmoid(value):
+    D = 1 + np.exp(-value)
+    sigmoid_value = 1 / D
+    return sigmoid_value
 
 def normalize_weight(G):
     for u in G.nodes():

probabilistic_algorithm.py
@@ -4,7 +4,10 @@ import networkx as nx
 import numpy as np
 from scipy import sparse
 
-def eruption_trivector(G, id_vent):
+def eruption_trivector(G, id_vent, threshold):
+#####################################
+    #threshold = 0.001 #inserire come parametro di input
+#####################################
     root = utility.get_node_from_idvent(str(int(id_vent)-1))
     # Inizializzazione dei tre vettori temporali
     vect1 = np.zeros(len(G.nodes()))
@@ -19,13 +22,11 @@ def eruption_trivector(G, id_vent):
     # Iniziamo mettendo i vicini della root nella coda esterna
     for v in G.successors(root):
         support_queue.put(v)
-        print(support_queue.qsize())
 
     key_to_control = "trasmittance" # trasmittance o weight
     
     # Inizio ciclo esterno, cicla fin quando non abbiamo più nodi da esplorare
     while not support_queue.empty():
-        print("Nodi da visitare", support_queue.qsize())
         
         # Trasferisco i nuovi vicini alla coda interna per poterli calcolare
         while not support_queue.empty():
@@ -56,8 +57,9 @@ def eruption_trivector(G, id_vent):
                 partial = product1 * product2
                 partial *= (vect2[pred[alpha]] - vect1[pred[alpha]])
                 increment += (partial * G.edges[str(pred[alpha]), j_node][key_to_control])
+            
             # Soglia dell'incremento
-            if increment > 0.01:
+            if increment > threshold:
                 vect3[int(j_node)] = vect2[int(j_node)] + increment
                 # Inserisco in coda i successori di j in modo che verranno esplorati al prossimo ciclo
             
@@ -77,7 +79,7 @@ def eruption_trivector(G, id_vent):
         G.node[str(index)]['current_flow'] = value
     
     sparse_vect = sparse.csr_matrix(vect3)
-    sparse.save_npz("sparse/npvect_probErup_" + id_vent + ".npz", sparse_vect, compressed = True)
+    sparse.save_npz("sparse/trivector_" + id_vent + ".npz", sparse_vect, compressed = True)
     
     return G
 

utility.py
@@ -133,11 +133,9 @@ def unify_sims(id_vent, char):
     for filename in filelist:
         if int(id_vent) == id_from_not_n(filename) + 1:
             current_vent_files.append(filename)
-    #print("unisco i seguenti file:", current_vent_files)
-    
+            
     lines_to_write = []
     for f in current_vent_files:
-        #print("processing file", f)
         with open(simspath + f, 'r') as infile:
             for line in infile:
                 if not line in lines_to_write:
@@ -181,9 +179,24 @@ def get_id_node_from_scaledcoord(G, row, col):
     u = -1
     return u
 
-def vect_to_matrix(id_vent):
+def vect_to_matrix(id_vent, propagation_method): #aggiungere intero 1/2/3
+    #############################################
+    # propagation_method = 1 ---> trivector     #
+    #                    = 2 ---> eruption      #
+    #                    = 3 ---> proberuption #
+    #############################################
+    if propagation_method == 1:
+        vect_filename = "sparse/trivector_" + str(id_vent) + ".npz"
+        matrix_filename = "sparse/M_trivector_" + str(id_vent) + ".npz"
+    elif propagation_method == 2:
+        vect_filename = "sparse/eruption_" + str(id_vent) + ".npz"
+        matrix_filename = "sparse/M_eruption_" + str(id_vent) + ".npz"
+    elif propagation_method == 3:
+        vect_filename = "sparse/proberuption_" + str(id_vent) + ".npz"
+        matrix_filename = "sparse/M_proberuption_" + str(id_vent) + ".npz"
+
     M = np.zeros((91, 75), dtype=float)
-    sparse_vect = sparse.load_npz("sparse/npvect_probErup_" + str(id_vent) + ".npz")
+    sparse_vect = sparse.load_npz(vect_filename)
     array = sparse_vect.toarray()[0]
     G = load_graph()
 
@@ -194,7 +207,7 @@ def vect_to_matrix(id_vent):
             M[reg_row][reg_col] = array[int(u)]
 
     sparse_M = sparse.csr_matrix(M)
-    sparse.save_npz("sparse/sparseM_probErup_" + str(id_vent) + ".npz", sparse_M)
+    sparse.save_npz(matrix_filename, sparse_M)
 
 # ottiene una matrice da un vettore per poter esportare una matrice sparsa (serve a calcolare la metrica di fitting)
 def vect_to_matrix_old(id_vent): # cambiare in filename del vect
@@ -226,12 +239,14 @@ def MAE_metric(id_vent, unify_type):
         return
     if unify_type == "d":
         if not os.path.isfile(udsim_sparse_file):
-            unify_sims(id_vent, unify_type)
+            tmp_M = unify_sims(id_vent, unify_type)
+            mc.matrix_to_UTM(tmp_M, id_vent, unify_type)
         M_sim = sparse.load_npz(udsim_sparse_file)
         M_sim = M_sim.toarray() 
     else:
         if not os.path.isfile(ucsim_sparse_file):
-            unify_sims(id_vent, unify_type)
+            tmp_M = unify_sims(id_vent, unify_type)
+            mc.matrix_to_UTM(tmp_M, id_vent, unify_type)
         M_sim = sparse.load_npz(ucsim_sparse_file)
         M_sim = M_sim.toarray()
 
@@ -240,7 +255,6 @@ def MAE_metric(id_vent, unify_type):
     ######################### calcolo la differenza tra matrici e errore cumulativo ########################################
     cumulative_err = 0
     error_matrix = np.zeros((M_sim.shape[0], M_sim.shape[1]), dtype='float')
-    #error_matrix = np.subtract(M_sim, M_prob)
     for row in range(0, M_sim.shape[0]):
         for col in range(0, M_sim.shape[1]):
             error = abs(M_sim[row][col] - M_prob[row][col])
@@ -269,6 +283,123 @@ def hit_metric(id_vent):
                 count_inters += 1
             if M_sim_sparse[row][col] > 0 or M_graphlow[row][col] > 0:
                 count_union += 1
-    
     hit = count_inters / count_union
     print("HIT metric:", hit, "\n")
+
+'''
+   | eruption | real  |
+TP |     V    |   V   |
+TN |     X    |   X   |
+FP |     V    |   X   |
+FN |     X    |   V   |
+'''
+def get_confusion_matrix_indexes(id_vent, propagation_method):
+    #############################################
+    # propagation_method = 1 ---> trivector     #
+    #                    = 2 ---> eruption      #
+    #                    = 3 ---> proberuption #
+    #############################################
+    if propagation_method == 1:
+        graphlow_sparse_file = "sparse/M_trivector_" + str(id_vent) + ".npz"
+    elif propagation_method == 2:
+        graphlow_sparse_file = "sparse/M_eruption_" + str(id_vent) + ".npz"
+    elif propagation_method == 3:
+        graphlow_sparse_file = "sparse/M_proberuption_" + str(id_vent) + ".npz"
+    
+    M_graphlow = sparse.load_npz(graphlow_sparse_file).toarray()
+    # real[row][col] = 0 o 1
+    udsim_sparse_file = "sparse/sparse_sim_d_" + str(id_vent) + ".npz"
+    # real[row][col] = TRA 0 e 1
+    ucsim_sparse_file = "sparse/sparse_sim_c_" + str(id_vent) + ".npz"
+    
+    if not os.path.isfile(udsim_sparse_file):
+        sparse_matrix = unify_sims(id_vent, 'd')
+        mc.matrix_to_UTM(sparse_matrix, id_vent, 'd')
+    if not os.path.isfile(ucsim_sparse_file):
+        unify_sims(id_vent, 'c')
+        mc.matrix_to_UTM(sparse_matrix, id_vent, 'c')
+        
+    M_real_d = sparse.load_npz(udsim_sparse_file).toarray()
+    M_real_c = sparse.load_npz(ucsim_sparse_file).toarray()
+    
+
+    fp = 0      # Contatore di fp booleano
+    tp = 0      # Contatore di tp booleano
+    tn = 0      # Contatore di tn booleano
+    fn = 0      # Contatore di fn booleano
+    
+    '''
+    fp_c = 0    # ACCUMULATORE fp
+    tp_c = 0    # ACCUMULATORE tp
+    tn_c = 0    # ACCUMULATORE tn
+    fn_c = 0    # ACCUMULATORE fn
+    '''
+
+    # Errori per MAE
+    cumulative_err_d = 0
+    cumulative_err_c = 0
+
+    for row in range(0, M_graphlow.shape[0]):
+        for col in range(0, M_graphlow.shape[1]):
+            if M_graphlow[row][col] > 0 and M_real_d[row][col] > 0:     # tp
+                tp += 1
+            if M_graphlow[row][col] > 0 and M_real_d[row][col] == 0:    # fp
+                fp += 1
+            if M_graphlow[row][col] == 0 and M_real_d[row][col] == 0:   # tn
+                tn += 1
+            if M_graphlow[row][col] == 0 and M_real_d[row][col] > 0:   # fn 
+                fn += 1
+
+            error_d = abs(M_real_d[row][col] - M_graphlow[row][col])
+            error_c = abs(M_real_c[row][col] - M_graphlow[row][col])
+            cumulative_err_d += error_d
+            cumulative_err_c += error_c
+
+    ################# calcolo la metrica MAE ########################  
+    mae_d = cumulative_err_d/(M_real_d.shape[0] * M_real_d.shape[1])#
+    mae_c = cumulative_err_c/(M_real_c.shape[0] * M_real_c.shape[1])#
+    #################################################################
+    return tp, tn, fp, fn, mae_d, mae_c
+
+# precision = PPV = tp/(tp + fp)
+def ppv(tp, fp):
+    precision = tp/ (tp + fp)
+    return precision
+
+# TPR = RECALL = tp/(tp + fn)
+def tp_rate(tp, fn):
+    tpr = tp/ (tp + fn)
+    return tpr
+
+# hit_rate = tp / (fp + fn + tp)
+def hit_rate(tp, fp, fn):
+    hit = tp / (fp + fn + tp)
+    return hit
+
+# acc = (tp + tn) / (tp + fp + tn + fn)
+def accuracy(tp, tn, fp, fn):
+    acc = (tp + tn) / (tp + fp + tn + fn)
+    return acc
+
+def f1(precision, tpr):
+    f1 = 2 * precision * tpr / (precision + tpr)
+    return f1
+
+def compute_metrics(id_vent, propagation_method):
+    tp, tn, fp, fn, mae_d, mae_c = get_confusion_matrix_indexes(id_vent, propagation_method)
+    precision = ppv(tp, fp)
+    #print("\nPPV =", precision)
+    
+    tpr = tp_rate(tp, fn)
+    #print("\nTPR =", tpr)
+    
+    acc = accuracy(tp, tn, fp, fn)
+    #print("\nacc = ", acc)
+    
+    hit = hit_rate(tp, fp, fn)
+    #print("\nhit = ", hit)
+    
+    #print("\nmae_d = ", mae_d)
+    #print("\nmae_c = ", mae_c)
+    F1 = f1(precision,tpr)
+    return precision, tpr, acc, hit, mae_d, mae_c, F1